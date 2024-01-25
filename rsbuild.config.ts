303a438036e4d2fd2ae892484c14f1d749d5717f
danilkk <ezzfvkoko@gmail.com>
2024-01-25 20:14:10 +0300

    some updates

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         },
     },
     output: {
-        assetPrefix: '/',
+        assetPrefix: '/tourism/',
         distPath: {
             image: 'assets',
             svg: 'assets',
