36015115e4190de830484f3ccff945f01ece0322
danilkk <ezzfvkoko@gmail.com>
2024-01-25 20:16:44 +0300

    some updates

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 
 import Home from "@/pages/Home/Home";
 import {default as DomAvseevih} from "@/pages/DomAvseevih/Page";
 import {default as SoborNeporochnogoZachatiyaPresvyatoyDeviMarii} from "@/pages/SoborNeporochnogoZachatiyaPresvyatoyDeviMarii/Page";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
         {
             path: "/dom-avseevih",
             element: <DomAvseevih/>,
